d0a68eee28f070dbf9308428ae22523f3993afd4
Phat Ky <phat.x.ky.-nd@disney.com>
2020-09-28 11:51:51 -0400

    add member function

--- lib/util.ml
+++ lib/util.ml
@@ -1,13 +1,12 @@
 
-
-
-module Member = ...
-
-let member name (y: Yaml.value) = 
-  let
-
-  match y with
-  | `O assoc -> OK assoc
-  | _ -> Error ???
+let member name (yl: Yaml.value) = 
+  match yl with
+  | `Null -> Error "Null yaml value"
+  | `Bool b -> Ok b
+  | `Float f -> Ok f
+  | `String s -> Ok s
+  | `A [ y ] -> Ok [ y ]
+  | `O assoc -> Ok assoc 
+  | yl -> Error "Unknown object: " ^ unk
 
 
