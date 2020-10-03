ba14a8d8a9c61a93e879061f28b1b7f11cdf6ccd
Phat Ky <phat.x.ky.-nd@disney.com>
2020-10-02 20:51:20 -0400

    work on member function

--- lib/util.ml
+++ lib/util.ml
@@ -1,12 +1,58 @@
 
-let member name (yl: Yaml.value) = 
+open Rresult
+
+
+module type M = sig 
+  type v
+  type al
+  type 'a res 
+  val to_value : string -> al -> v
+  val from_value : string -> v -> (v*string) res 
+  val member : string -> v -> (v*string) res  
+end 
+
+module Member = struct
+  type v = Yaml.value
+  type al = (string*v) list
+  type 'a res = (v,string) result
+  let to_value n al = List.assoc n al 
+
+end
+
+
+
+module M : sig 
+  type v
+  type al
+  type 'a res 
+  val to_value : string -> al -> v
+  val member : string -> v -> (v*string) res  
+end = struct
+  type v = Yaml.value
+  type al = (string*v) list
+  type 'a res = (v,string) result
+  let to_value n al = List.assoc n al 
+  let member n ym = 
+    match ym with
+    | `O al -> Ok (to_value n al)
+    | ym -> Error "Not a Yaml Value"
+end
+
+let rec get_value name assoclist =
+  let v = List.get_value name assoclist in
+  match v with
+  | `Null -> Error "No Value for this key"
+  | `Bool b -> Ok (`Bool b)
+  | `Float f -> Ok (`Float f)
+  | `String s -> Ok (`String s)
+  | `A v -> Ok (`A v)
+  | `O al -> get_value al 
+  | _ -> Error "Unknown key"
+
+let member (name:string) (yl: Yaml.value) = 
   match yl with
-  | `Null -> Error "Null yaml value"
-  | `Bool b -> Ok b
-  | `Float f -> Ok f
-  | `String s -> Ok s
-  | `A [ y ] -> Ok [ y ]
-  | `O assoc -> Ok assoc 
-  | yl -> Error "Unknown object: " ^ unk
+  | `O y -> get_value y 
+  | yl -> Error "Not a Yaml value" 
+
 
 
