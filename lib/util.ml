77b825657ba5e57366ac7d205559e1ca609a9cb5
phage <kyp0717@gmail.com>
2020-10-04 08:26:14 -0400

    clean up member function

--- lib/util.ml
+++ lib/util.ml
@@ -2,26 +2,7 @@
 open Rresult
 
 
-module type M = sig 
-  type v
-  type al
-  type 'a res 
-  val to_value : string -> al -> v
-  val from_value : string -> v -> (v*string) res 
-  val member : string -> v -> (v*string) res  
-end 
-
-module Member = struct
-  type v = Yaml.value
-  type al = (string*v) list
-  type 'a res = (v,string) result
-  let to_value n al = List.assoc n al 
-
-end
-
-
-
-module M : sig 
+module Member : sig 
   type v
   type al
   type 'a res 
@@ -38,21 +19,8 @@ end = struct
     | ym -> Error "Not a Yaml Value"
 end
 
-let rec get_value name assoclist =
-  let v = List.get_value name assoclist in
-  match v with
-  | `Null -> Error "No Value for this key"
-  | `Bool b -> Ok (`Bool b)
-  | `Float f -> Ok (`Float f)
-  | `String s -> Ok (`String s)
-  | `A v -> Ok (`A v)
-  | `O al -> get_value al 
-  | _ -> Error "Unknown key"
 
-let member (name:string) (yl: Yaml.value) = 
-  match yl with
-  | `O y -> get_value y 
-  | yl -> Error "Not a Yaml value" 
+
 
 
 
