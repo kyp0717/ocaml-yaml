7fb2d2a2f0034fffcca77bfea62dd912334b15a0
phage <kyp0717@gmail.com>
2020-11-02 20:55:20 -0500

    unit test for member function

--- tests/test_util.ml
+++ tests/test_util.ml
@@ -10,13 +10,13 @@ let error = Alcotest.testable pp_error ( = )
 [@@@part "1"]
 
 let test_member () =
-  let open Yaml in
+  (* let open Yaml in *)
   let ok_input = `O [ ("author", `String "Alice") ] in
-  let ok_output = Ok (`String "Alice") in
+  let ok_output = Ok (Some (`String "Alice") ) in
   let err_input = `String "Test_Error" in
-  let err_output = `Msg "Expected an `O"  in
-  Alcotest.(check (result yaml error)) "success" ok_output (Yaml.Util.member "author" ok_input);
-  Alcotest.(check (result yaml error)) "fail" err_output (Yaml.Util.member "author" err_input)
+  let err_output = Error (`Msg "Not an `0") in
+  Alcotest.(check (result (option yaml) error)) "success" ok_output (Yaml.Util.member "author" ok_input);
+  Alcotest.(check (result (option yaml) error))  "fail" err_output (Yaml.Util.member "author" err_input)
 
 [@@@part "2"]
 
